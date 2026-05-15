9037237e581624f8fa4fa7cdf89b0ec55832f072
Josh Cunningham <josh@joshcanhelp.com>
2026-05-15 14:47:07 -0700

    feat: add get

---
+++ get.ts
@@ -0,0 +1,19 @@
+import { getAccessToken, getEnv } from "./mgmt.ts";
+
+const { domain, clientId, clientSecret } = getEnv();
+
+const access_token = await getAccessToken(domain, clientId, clientSecret);
+
+const res = await fetch(`https://${domain}/api/v2/branding/templates/universal-login`, {
+  headers: {
+    "Authorization": `Bearer ${access_token}`,
+  },
+});
+
+if (!res.ok) {
+  console.error("Failed to get template:", await res.text());
+  process.exit(1);
+}
+
+const { body } = await res.json() as { body: string };
+console.log(body);
